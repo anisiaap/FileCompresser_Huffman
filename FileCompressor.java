18c4a80721a304657ed4433c9bf95f6d547f8df5
anisiaap <pirvulescuanisiaa@gmail.com>
2025-04-12 20:30:43 +0300

    cmd args read

---
+++ FileCompressor.java
@@ -0,0 +1,14 @@
+public class FileCompressor {
+    public static void main(String[] args) {
+        if (args.length != 3) {
+            System.err.println("Not enough argcs, please use : java FileCompresser mode inputFile outputFile");
+            System.exit(1);
+
+            String mode = args[0];
+            String inputFile = args[1];
+            String outputFile = args[2];
+
+            System.out.println(mode + " " +  inputFile+  " "+  outputFile);
+        }
+    }
+}
